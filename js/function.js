b2fb06771810bdac796af3b1faecca9ca56cb71f
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-03-14 15:23:59 +1100

    new changes applied to the page

--- js/function.js
+++ js/function.js
@@ -17,23 +17,3 @@ function checktime(i) {
 
     return i;
 }
-// Code above is just repeated
-
-function doSomeThing() {  // This is function declaration syntex
-    let today = new Date();
-    let h = today.getHours();
-    let m = today.getMinutes();
-    let s = today.getSeconds();
-    m = checktime(m);
-    s = checktime(s);
-    document.getElementById('test2').innerHTML = h + ":" + m + ":" + s;
-
-    let = setTimeout(doSomeThing, 500);
-
-}
-
-function checktime(i) {
-    if (i < 10) { i = "0" + i };
-
-    return i;
-}
