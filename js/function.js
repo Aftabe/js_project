34dcd803c93559cc551d010334f0d79d466ce5b7
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-03-14 15:22:02 +1100

    a block added

--- js/function.js
+++ js/function.js
@@ -19,3 +19,21 @@ function checktime(i) {
 }
 // Code above is just repeated
 
+function doSomeThing() {  // This is function declaration syntex
+    let today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checktime(m);
+    s = checktime(s);
+    document.getElementById('test2').innerHTML = h + ":" + m + ":" + s;
+
+    let = setTimeout(doSomeThing, 500);
+
+}
+
+function checktime(i) {
+    if (i < 10) { i = "0" + i };
+
+    return i;
+}
