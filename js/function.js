2d2c5f53cd463d1302cf6e9fc3c89384aae29f92
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-03-18 18:43:17 +1100

    changes applied

--- js/function.js
+++ js/function.js
@@ -1,5 +1,5 @@
 // Setting a clock using js function
-function startTime(){  // This is function declaration syntex
+function startTime() {  // This is function declaration syntex
     let today = new Date();
     let h = today.getHours();
     let m = today.getMinutes();
@@ -12,10 +12,42 @@ function startTime(){  // This is function declaration syntex
 
 }
 
-function checktime(i){
-    if(i < 10) {i = "0" + i};
+function checktime(i) {
+    if (i < 10) { i = "0" + i };
 
     return i;
+<<<<<<< HEAD
 };
 
-// Selecting an element from html page and modify them
+// Selecting an element from html page and modify the
+// Switch method
+const paymenthMethod = 'cash';
+
+switch(paymenthMethod){
+    case 'cash':
+        alert(`your paymenth method is : ${paymenthMethod}`);
+        break;
+    case 'card':
+        alert(`your paymenth method is : ${paymenthMethod} we are verifying your card`);
+        break;
+        case 'check':
+            alert(`your paymenth method is : ${paymenthMethod} we are processing your check`);
+        break;
+};
+// This is the second example of 'Switch statement'
+const cars = ['Ford','Toyota','Mazda'];
+
+let seclectCar = 2;
+let car;
+
+switch(seclectCar) {
+    case 0:
+        car = cars[0];
+        break;
+    case 1:
+        car = cars[1];
+        break;
+    case 2:
+        car = cars[2];
+        break;
+}
